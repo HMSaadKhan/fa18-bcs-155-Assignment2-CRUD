4b4db3b66b5cf246262ea939d965cd9193b8b10a
Saad_Khan <Khan.ji4028@gmail.com>
2021-06-18 22:50:19 +0500

    update

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var mongoose = require('mongoose');
+var config = require('config');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var usersRouter = require('./routes/api/users');
+var merchRouter = require('./routes/api/merch');
 var app = express();
 
 // view engine setup
@@ -20,8 +22,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
+app.use('/api/users', usersRouter);
+app.use('/api/merch', merchRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -37,5 +39,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+mongoose.connect(config.get("db"),{ useNewUrlParser: true, useUnifiedTopology: true })
+.then(async ()=> {
+  console.log("database connection created");
+ 
+}).catch((error)=>console.log(error.message));
 module.exports = app;
